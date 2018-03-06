6bf54c00276862460416bd235382595f2b453886
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-06 12:52:21 +0800

    Update advroll.js

--- roll/advroll.js
+++ roll/advroll.js
@@ -19,7 +19,7 @@ function d66(text) {
 
 function xyxy(x1,y1,x2,y2,text) {
 	var temp =0;
-	temp = Math.pow(Math.pow(x2-x1,2)+Math.pow(y2-y1,2),0.5);
+	temp = Math.floor(Math.pow(Math.pow(x2-x1,2)+Math.pow(y2-y1,2),0.5));
 	let returnStr = '';	if(text != null){
 	returnStr =	'距離為：'  + ' → ' +temp +' ' + text;
 	}
