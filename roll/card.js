d3de153af5e37d5cba5bddd13203a011d5e1029c
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-06 19:54:15 +0800

    Update card.js

--- roll/card.js
+++ roll/card.js
@@ -12,46 +12,46 @@ return rply;
 }
 function N() {
 let rplyArr = [
-'\編號01:N鼠的畫', 
-'\編號02:N雷洛的程式', 
-'\編號03:N一定墜落的翅膀',
-'\編號04:N雷銘的筆',
-'\編號05:N夜月的肝',
-'\編號06:N木劍',
-'\編號07:R偽裝成R的N',
-'\編號08:SR偽裝成SR的N',
-'\編號09:SSR偽裝成SSR的N'
+'\[01] [N] 鼠的畫', 
+'\[02] [N] 雷洛的程式', 
+'\[03] [N] 一定墜落的翅膀',
+'\[04] [N] 雷銘的筆',
+'\[05] [N] 夜月的肝',
+'\[06] [N] 木劍',
 ];
 rply.text = rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 return rply;	
 }
 function R() {
 let rplyArr = [
-'\編號11:R鐵劍', 
-'\編號12:R炎刃', 
-'\編號13:R風之弓',
-'\編號14:R海之鞭',
-'\編號15:R地之杖'
+'\[07] [N] 偽裝成[R]的[N]',
+'\[11] [R] 鐵劍', 
+'\[12] [R] 炎刃', 
+'\[13] [R] 風之弓',
+'\[14] [R] 海之鞭',
+'\[15] [R] 地之杖'
 ];
 rply.text = rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 return rply;	
 }
 function SR() {
 let rplyArr = [
-'\編號91:SR風之劍', 
-'\編號92:SR火之劍', 
-'\編號93:SR海之劍',
-'\編號94:SR地之劍'
+'\[08] [N] 偽裝成[SR]的[N]',
+'\[21] [SR] 風之劍', 
+'\[22] [SR] 火之劍', 
+'\[23] [SR] 海之劍',
+'\[24] [SR] 地之劍'
 ];
 rply.text = rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 return rply;	
 }
 function SSR() {
 let rplyArr = [
-'\編號101:SSR風神', 
-'\編號102:SSR火山之神', 
-'\編號103:SSR海洋之神',
-'\編號104:SSR大地之神'
+'\[09] [N] 偽裝成[SSR]的[N]',
+'\[31] [SSR] 風神', 
+'\[32] [SSR] 火山之神', 
+'\[33] [SSR] 海洋之神',
+'\[34] [SSR] 大地之神'
 ];
 rply.text = rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 return rply;	
