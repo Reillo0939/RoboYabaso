2caed64192c11b96ba196e43cbb3a19ae98c3566
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-07 19:51:24 +0800

    Update Character.js

--- roll/Character.js
+++ roll/Character.js
@@ -4,24 +4,23 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 
 function Character(name,age) {
 
-var HP,MP,ATK,None,Fire,Water,Wind,Earth;
-var Points = 0;
-do {
-    if((rollbase.Dice(100)>=50)&& (HP<200) && (Points<100)){HP+=5;Points++;}
-    if((rollbase.Dice(100)>=50)&& (MP<200) && (Points<100)){MP+=5;Points++;}
-    if((rollbase.Dice(100)>=50)&& (ATK<50) && (Points<100)){ATK++;Points++;}
-    if((rollbase.Dice(100)>=50)&& (None<50) && (Points<100)){None++;Points++;}
-    if((rollbase.Dice(100)>=50)&& (Fire<50) && (Points<100)){Fire++;Points++;}
-    if((rollbase.Dice(100)>=50)&& (Water<50) && (Points<100)){Water++;Points++;}
-    if((rollbase.Dice(100)>=50)&& (Wind<50) && (Points<100)){Wind++;Points++;}
-    if((rollbase.Dice(100)>=50)&& (Earth<50) && (Points<100)){Earth++;Points++;}
-} while (Points < 10);
+var HP,MP,ATK,None,Fire,Water,Wind,Earth,Reaction;
+HP=((rollbase.Dice(20) - 1) * 9)+20;
+MP=((rollbase.Dice(20) - 1) * 9)+20;
+ATK=rollbase.Dice(50);
+Reaction=rollbase.Dice(50);
+None=rollbase.Dice(50);
+Fire=rollbase.Dice(50);	
+Water=rollbase.Dice(50);
+Wind=rollbase.Dice(50);
+Earth=rollbase.Dice(50);
 
 rply.text=
 '['+ name +']  年齡：' +age +
 '\n生命值： '+ HP +
 '\nBata粒子適性： '+ MP +
 '\n物理適性： '+ ATK +
+'\n反應力： '+ Reaction +
 '\n放出適性： '+ None +
 '\n火屬適性： '+ Fire +
 '\n水屬適性： '+ Water +
