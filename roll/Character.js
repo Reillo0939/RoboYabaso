a29f6ae2a4e15252fcff1461388cebdf82eadd8d
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-07 19:44:12 +0800

    Update Character.js

--- roll/Character.js
+++ roll/Character.js
@@ -1,6 +1,6 @@
 var rollbase = require('./rollbase.js');
 var funny = require('./funny.js');
-var rply ={type : 'text'}; //type是必需的,但可以更改1
+var rply ={type : 'text'}; //type是必需的,但可以更改
 
 function Character(name,age) {
 
