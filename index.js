e835e218c918743034f5e0fc2c73f115964f2ce6
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-07 23:57:36 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -23,6 +23,34 @@ bot.on('message', function (event) {
 bot.listen('/linewebhook', 5000);
 // Load `*.js` under modules directory as properties
 //  i.e., `User.js` will become `exports['User']` or `exports.User`
+
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+//以下的引號內請輸入申請LineBot取得的各項資料，逗號及引號都不能刪掉
+var bot = linebot({
+  channelId: '請輸入LineBot的channelId',
+  channelSecret: '請輸入LineBot的channelSecret',
+  channelAccessToken: '請輸入LineBot的channelAccessToken'
+});
+
+//底下輸入client_secret.json檔案的內容
+var myClientSecret={"installed":{"client_id":"399740110786-ai6tcngsubr5d8jc1qdirv5b1ehmft9h.apps.googleusercontent.com","project_id":"linebot-0939","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"z9gr8MowvkKKI_xI7HfaunSO","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
+
+var auth = new googleAuth();
+var oauth2Client = new auth.OAuth2(myClientSecret.installed.client_id,myClientSecret.installed.client_secret, myClientSecret.installed.redirect_uris[0]);
+
+//試算表的ID，引號不能刪掉
+var mySheetId='請輸入試算表的ID編號';
+
+var title='';
+var users=[];
+
+var totalSteps=0;
+var myQuestions=[];
+
+
+
 require('fs').readdirSync(__dirname + '/modules/').forEach(function(file) {
   if (file.match(/\.js$/) !== null && file !== 'index.js') {
     var name = file.replace('.js', '');
