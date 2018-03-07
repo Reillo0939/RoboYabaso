93925c9998d8d12232facf0fe6f8920634ca8667
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-07 21:09:14 +0800

    Update card.js

--- roll/card.js
+++ roll/card.js
@@ -35,68 +35,80 @@ return rply;
 }
 function N() {
 let rplyArr = [
-'\[001] [N] 鼠的畫', 
-'\[002] [N] 雷洛的程式', 
-'\[003] [N] 一定墜落的翅膀',
-'\[004] [N] 雷銘的筆',
-'\[005] [N] 夜月的肝',
-'\[006] [N] 木劍',
-'\[007] [N] 不是史萊姆',
-'\[008] [N] 火之庇護',
-'\[009] [N] 水之庇護',
-'\[010] [N] 風之庇護',
-'\[011] [N] 土之庇護'
+'\[001] [N] [Item] 鼠的畫', 
+'\[002] [N] [Item] 雷洛的程式', 
+'\[003] [N] [Item] 一定墜落的翅膀',
+'\[004] [N] [Item] 雷銘的筆',
+'\[005] [N] [Item] 夜月的肝',
+'\[006] [N] [Item] 木劍',
+'\[007] [N] [Accessories] 火之庇護',
+'\[008] [N] [Accessories] 水之庇護',
+'\[009] [N] [Accessories] 風之庇護',
+'\[010] [N] [Accessories] 土之庇護',
+'\[011] [N] [Skill] 熱量控制',
+'\[012] [N] [Skill] 燃燒現象',
+'\[013] [N] [Skill] 質能轉換-水',
+'\[014] [N] [Skill] 水流控制',
+'\[015] [N] [Skill] 簡單氣壓控制',
+'\[016] [N] [Skill] 強制位移',
+'\[017] [N] [Skill] 密度控制-物',
+'\[018] [N] [Skill] 土牆建立'
 ];
 rply.text = rply.text +'\n'+ rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 return rply;	
 }
 function R() {
 let rplyArr = [
-'\[030] [N] 偽裝成[R]的[N]卡',
-'\[031] [R] 石劍', 
-'\[032] [R] 炎之劍', 
-'\[033] [R] 風之弓',
-'\[034] [R] 海之鞭',
-'\[035] [R] 地之杖',
-'\[036] [R] 鼠的畫ex',
-'\[037] [R] 雷洛那沒bug的程式',
-'\[038] [R] 翅膀那雷神',
-'\[039] [R] 雷銘的0.2mm自動鉛筆',
-'\[040] [R] 夜月那快撐不住的肝'
+'\[030] [N] [Item] 偽裝成[R]的[N]卡',
+'\[031] [R] [Item] 鼠的畫ex',
+'\[032] [R] [Item] 雷洛那沒bug的程式',
+'\[033] [R] [Item] 翅膀那雷神',
+'\[034] [R] [Item] 雷銘的0.2mm自動鉛筆',
+'\[035] [R] [Item] 夜月那快撐不住的肝'.
+'\[036] [R] [Skill] 熱距離延長',
+'\[037] [R] [Skill] 燃燒現象強化',
+'\[038] [R] [Skill] 水壓提升(小)',
+'\[039] [R] [Skill] 造霧',
+'\[040] [R] [Skill] 噴氣移動',
+'\[041] [R] [Skill] 位移加速',
+'\[042] [R] [Skill] 沙漠化',
+'\[043] [R] [Skill] 流沙'
 ];
 rply.text = rply.text +'\n'+ rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 return rply;	
 }
 function SR() {
 let rplyArr = [
-'\[060] [N] 偽裝成[SR]的[N]卡',
-'\[061] [SR] 颶風之弓', 
-'\[062] [SR] 烈火之劍', 
-'\[063] [SR] 大海之鞭',
-'\[064] [SR] 大地魔杖',
-'\[065] [SR] 鐵劍',
-'\[066] [SR] 鼠那超進步的畫',
-'\[067] [SR] 雷洛那超簡潔的程式',
-'\[068] [SR] 翅膀雷好雷滿',
-'\[069] [SR] 雷銘的用不壞的平板',
-'\[070] [SR] 夜月那超越人類的肝'
+'\[060] [N] [Item] 偽裝成[SR]的[N]卡',
+'\[061] [SR] [Item] 鼠那超進步的畫',
+'\[062] [SR] [Item] [Item] 雷洛那超簡潔的程式',
+'\[063] [SR] [Item] 翅膀雷好雷滿',
+'\[064] [SR] [Item] 雷銘的用不壞的平板',
+'\[065] [SR] [Item] 夜月那超越人類的肝',
+'\[066] [SR] [Skill] 燃燒標記',
+'\[067] [SR] [Skill] 創水控制',
+'\[068] [SR] [Skill] 風壓推移',
+'\[069] [SR] [Skill] 點石成金'
 ];
 rply.text = rply.text +'\n' + rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 return rply;	
 }
 function SSR() {
 let rplyArr = [
-'\[090] [N] 偽裝成[SSR]的[N]卡',
-'\[091] [SSR] 颶風之神', 
-'\[092] [SSR] 烈火之神', 
-'\[093] [SSR] 大海之神',
-'\[094] [SSR] 大地之神',
-'\[095] [SSR] 鑽石劍',
-'\[096] [SSR] 鼠',
-'\[097] [SSR] 雷洛',
-'\[098] [SSR] Wings',
-'\[099] [SSR] 雷銘',
-'\[100] [SSR] 夜月'
+'\[090] [N] [Item] 偽裝成[SSR]的[N]卡',
+'\[091] [SSR] [Accessories] 颶風之神', 
+'\[092] [SSR] [Accessories] 烈火之神', 
+'\[093] [SSR] [Accessories] 大海之神',
+'\[094] [SSR] [Accessories] 大地之神',
+'\[095] [SSR] [Partner] 鼠',
+'\[096] [SSR] [Partner] 雷洛',
+'\[097] [SSR] [Partner] Wings',
+'\[098] [SSR] [Partner] 雷銘',
+'\[099] [SSR] [Partner] 夜月',
+'\[100] [SR] [Skill] 萬物燃盡',
+'\[101] [SR] [Skill] 水刃劃出',
+'\[102] [SR] [Skill] 風壓炸裂',
+'\[103] [SR] [Skill] 大地石封'
 ];
 rply.text = rply.text +'\n'+ rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 return rply;	
