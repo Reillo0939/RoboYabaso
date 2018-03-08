3e91bcddc36d523acf4284344cc9b6a9541024d5
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-08 22:12:13 +0800

    Update Character.js

--- roll/Character.js
+++ roll/Character.js
@@ -15,32 +15,6 @@ Fire=rollbase.Dice(70-age);
 Water=rollbase.Dice(70-age);
 Wind=rollbase.Dice(70-age);
 Earth=rollbase.Dice(70-age);
-}
-if((age>=14)&&(age<=29)){
-Occupation='放出使';
-HP=((rollbase.Dice(20) - 1) * 9)+20;
-MP=((rollbase.Dice(20) - 1) * 9)+20;
-ATK=rollbase.Dice(50);
-Reaction=rollbase.Dice(50);
-None=rollbase.Dice(50);
-Fire=rollbase.Dice(50);	
-Water=rollbase.Dice(50);
-Wind=rollbase.Dice(50);
-Earth=rollbase.Dice(50);
-}
-if((age<14)||(age>65)){
-Occupation='不適合戰鬥者';
-HP=rollbase.Dice(20) ;
-MP=rollbase.Dice(20) ;
-ATK=rollbase.Dice(5);
-Reaction=rollbase.Dice(5);
-None=rollbase.Dice(5);
-Fire=rollbase.Dice(5);	
-Water=rollbase.Dice(5);
-Wind=rollbase.Dice(5);
-Earth=rollbase.Dice(5);
-}
-
 rply.text=
 '['+ name +']  年齡：' +age +
 '\n職業：  ' + Occupation +
@@ -53,12 +27,54 @@ rply.text=
 '\n水屬適性： '+ Water +
 '\n風屬適性： '+ Wind +
 '\n土屬適性： '+ Earth ;
+}
+if((age>=14)&&(age<=29)){
+Occupation='放出使';
+HP=((rollbase.Dice(20) - 1) * 9)+20;
+MP=((rollbase.Dice(20) - 1) * 9)+20;
+ATK=rollbase.Dice(50);
+Reaction=rollbase.Dice(50);
+None=rollbase.Dice(50);
+Fire=rollbase.Dice(50);	
+Water=rollbase.Dice(50);
+Wind=rollbase.Dice(50);
+Earth=rollbase.Dice(50);
+rply.text=
+'['+ name +']  年齡：' +age +
+'\n職業：  ' + Occupation +
+'\n生命值： '+ HP +
+'\nBata粒子適性： '+ MP +
+'\n物理適性： '+ ATK +
+'\n反應力： '+ Reaction +
+'\n放出適性： '+ None +
+'\n火屬適性： '+ Fire +
+'\n水屬適性： '+ Water +
+'\n風屬適性： '+ Wind +
+'\n土屬適性： '+ Earth ;
+}
+if((age<14)||(age>65)){
+Occupation='不適合戰鬥者';
+HP=rollbase.Dice(20) ;
+MP=rollbase.Dice(20) ;
+ATK=rollbase.Dice(5);
+Reaction=rollbase.Dice(5);
+None=rollbase.Dice(5);
+Fire=rollbase.Dice(5);	
+Water=rollbase.Dice(5);
+Wind=rollbase.Dice(5);
+Earth=rollbase.Dice(5);
+rply.text=
+'['+ name +']  年齡：' +age +
+'\n職業：  ' + Occupation +
+}
+
+
 return rply;	
 }
 function CT(name,age) {
 	var HP,MP,ATK,Reaction,Occupation,Control;
 if((age>=40)&&(age<=60)){
-Occupation='較不適合戰鬥者';
+Occupation='外部裝甲操縱人員';
 HP=((rollbase.Dice(20) - 1) * 9)+20;
 MP=((rollbase.Dice(5) - 1) * 9)+20;
 ATK=rollbase.Dice(70-age);
@@ -81,7 +97,7 @@ ATK=rollbase.Dice(5);
 Reaction=rollbase.Dice(5);
 Control=rollbase.Dice(5);
 }
-
+if((Control<10)||(MP<50))Occupation='外骨骼裝備步兵';
 rply.text=
 '['+ name +']  年齡：' +age +
 '\n職業：  ' + Occupation +
