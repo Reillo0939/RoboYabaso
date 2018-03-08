ff7ff37ec3a8c3594f45a4dfaf142d6d55f5147d
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-08 21:53:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -49,13 +49,13 @@ var cat;
  sheets.spreadsheets.values.get({
 auth: API_KEY,
   spreadsheetId: '1QUIuFsRa1PP-862kS7TmwWSPxRrqhv5HBuu2n9tHIlg',
-  range: 'test'!A1:B1
+  range: 'A1:A1'
 }, function(err, result) {
   if(err) {
     // Handle error
     console.log(err);
   } else {
-	  cat =result.values
+	  cat =result.values[0][0];
     var numRows = result.values ? result.values.length : 0;
     console.log('%d rows retrieved.', numRows);
   }
