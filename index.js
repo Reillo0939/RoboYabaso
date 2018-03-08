43f61bc8b04fef658fff12a14122d264f9e901ed
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-08 21:16:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ var API_KEY = 'AIzaSyCEtwsTELMS5YtDw3A6LesTHvQ4OrElgGA'; // specify your API key
 //試算表的ID，引號不能刪掉
 var mySheetId='1QUIuFsRa1PP-862kS7TmwWSPxRrqhv5HBuu2n9tHIlg';
  var sheets = google.sheets('v4');
-var cat = sheets.spreadsheets.values.get({
+var cat.text = sheets.spreadsheets.values.get({
 auth: API_KEY,
   spreadsheetId: '1QUIuFsRa1PP-862kS7TmwWSPxRrqhv5HBuu2n9tHIlg',
   range: 'test!A1:A1'
