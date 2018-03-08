f465384d8f2b6d6b667e2a11928bfd275a105b01
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-08 14:12:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,51 +44,26 @@ var oauth2Client = new OAuth2(
 
 //試算表的ID，引號不能刪掉
 var mySheetId='1QUIuFsRa1PP-862kS7TmwWSPxRrqhv5HBuu2n9tHIlg';
-authorize(function(authClient) {
-  var request = {
-    // The ID of the spreadsheet to update.
-    spreadsheetId: mySheetId,  // TODO: Update placeholder value.
-
-    // The A1 notation of the values to update.
-    range: 'A1',  // TODO: Update placeholder value.
-
-    // How the input data should be interpreted.
-    valueInputOption: '01',  // TODO: Update placeholder value.
-
-    resource: {
-      // TODO: Add desired properties to the request body. All existing properties
-      // will be replaced.
-    },
-
-    auth: auth,
-  };
-
-  sheets.spreadsheets.values.update(request, function(err, response) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    // TODO: Change code below to process the `response` object:
-    console.log(JSON.stringify(response, null, 2));
-  });
-});
-
-function authorize(callback) {
-  // TODO: Change placeholder below to generate authentication credentials. See
-  // https://developers.google.com/sheets/quickstart/nodejs#step_3_set_up_the_sample
-  //
-  // Authorize using one of the following scopes:
-  //   'https://www.googleapis.com/auth/drive'
-  //   'https://www.googleapis.com/auth/drive.file'
-  //   'https://www.googleapis.com/auth/spreadsheets'
-  var authClient =null;
-
-  if (authClient == null) {
-    console.log('authentication failed');
-    return;
-  }
-  callback(authClient);
+function appendMyRow(userId) {
+   var request = {
+      auth: oauth2Client,
+      spreadsheetId: mySheetId,
+      range:encodeURI('test'),
+      insertDataOption: 'INSERT_ROWS',
+      valueInputOption: 'RAW',
+      resource: {
+        "values": [
+          '030'
+        ]
+      }
+   };
+   var sheets = google.sheets('v4');
+   sheets.spreadsheets.values.append(request, function(err, response) {
+      if (err) {
+         console.log('The API returned an error: ' + err);
+         return;
+      }
+   });
 }
 require('fs').readdirSync(__dirname + '/modules/').forEach(function(file) {
   if (file.match(/\.js$/) !== null && file !== 'index.js') {
