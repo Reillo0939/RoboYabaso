d3d0256cc7fa361c4dc325dbf854ab23392b7431
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-10 20:11:23 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ app.post('/', jsonParser, function(req, res) {
 	}
 	//把回應的內容,掉到replyMsgToLine.js傳出去
 	if (rplyVal) {
-		exports.replyMsgToLine.replyMsgToLine(rplyToken, rplyVal, options);
+		//exports.replyMsgToLine.replyMsgToLine(rplyToken, rplyVal, options);
 	
 		
 	} else {
