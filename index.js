aaa954529e87cdb7ef0700ac38d0958561ffa3ca
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-14 07:30:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ sheets.spreadsheets.values.get({
   } else {
     var numRows;
     numRows = response.values;
-    console.log('retrieved.   ' + numRows[0].length);
+    console.log('retrieved.   ' + numRows[1].length);
   }
 });
 b=profile.displayName;
