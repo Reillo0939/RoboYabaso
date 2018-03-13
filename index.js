5eaa8617aebd2f6802e75d66f3cff4e8882da8b9
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-13 22:51:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ event.source.profile().then(function (profile) {
 sheets.spreadsheets.values.get({
   auth: API_KEY,
       spreadsheetId: mySheetId,
-      range:'test!A1:A1',
+      range:'test!A1:A1'
 }, function(err, result) {
   if(err) {
     // Handle error
