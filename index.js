dc7ade9bd1b0ed6a1800f0496d03e3b3b3f0424e
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-14 08:36:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var google = require('googleapis');
 var sheets = google.sheets('v4');
 var googleAuth = require('google-auth-library');
 var auth = new googleAuth();
-//var OAuth2 = google.auth.OAuth2;
+var OAuth2 = google.auth.OAuth2;
 /*var oauth2Client = new auth.OAuth2(  '399740110786-ne3lhespt77ni2mt99ae1sunohrts5jh.apps.googleusercontent.com',
 'OzjUGCSYtQExFf4k_XJc9Q_W',
   'https://dream-realm-v2.herokuapp.com/oauth2calkback'
