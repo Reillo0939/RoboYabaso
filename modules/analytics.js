e660f483c68e0490db1e08578374e30785110553
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-19 22:59:42 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -94,8 +94,8 @@ function parseInput(rplyToken, inputStr, id,name) {
 	
 	if (trigger.match(/(^玩家權限$|^玩家權限$)/) != null) return exports.card.IDCA(id,name) ;
 	
-	if (trigger=='gU角色創立') return exports.Character.CM(mainMsg[1],mainMsg[2],id) ;
-	if (trigger=='AAUF角色創立') return exports.Character.CT(mainMsg[1],mainMsg[2],id) ;
+	if (trigger.match(/(^gU角色創立$)/) != null) return exports.Character.CM(mainMsg[1],mainMsg[2],id) ;
+	if (trigger.match(/(^AAUF角色創立$)/) != null) return exports.Character.CT(mainMsg[1],mainMsg[2],id) ;
 	
 	if (trigger.match(/^角色資料$/) != null) return exports.Character.CV(id,name) ;
 	if (trigger.match(/^角色查詢$/) != null) return exports.Character.CI(name,mainMsg[1]) ;
