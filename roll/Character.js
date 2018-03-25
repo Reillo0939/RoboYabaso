03f436151021724f38c63c566f854b99cef1554f
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-25 11:30:16 +0800

    Update Character.js

--- roll/Character.js
+++ roll/Character.js
@@ -444,7 +444,7 @@ sheets.spreadsheets.values.update(request, function(err, result) {
 function CCCN(auth) {
 var leng=ccN;
 	var values = [
-      [Characters[leng][0] ,Characters[leng][1],]
+      [Characters[leng+1][0] ,Characters[leng+1][1],]
 ];
 	console.log('test OK');
 	var range='Character!A' + leng;
