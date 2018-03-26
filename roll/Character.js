6914d41d1b9a3d34c2307eb28b43b07d502f6250
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-26 20:13:48 +0800

    Update Character.js

--- roll/Character.js
+++ roll/Character.js
@@ -361,11 +361,11 @@ if(Characters[fd][0]==id){
 				if(select=='物理適性' && Characters[fd][14]>=Points){
 					Characters[fd][7]=parseInt(Characters[fd][7],10)+parseInt(Points, 10);
 					Characters[fd][14]-=Points;
-					rply.text='分配成功';				}
+					rply.text='配置成功';				}
 				else if(select=='控制能力' && Characters[fd][14]>=Points){
 					Characters[fd][15]=parseInt(Characters[fd][15],10)+parseInt(Points, 10);
 					Characters[fd][14]-=Points;
-					rply.text='分配成功';				}
+					rply.text='配置成功';				}
 				else{rply.text='點數不足';}
 				fs.readFile('client_secret.json', function processClientSecrets(err, content) {
   				if (err) {
@@ -385,27 +385,27 @@ if(Characters[fd][0]==id){
 				if(select=='物理適性' && Characters[fd][14]>=Points){
 					Characters[fd][7]=parseInt(Characters[fd][7],10)+parseInt(Points, 10);
 					Characters[fd][14]-=Points;
-					rply.text='分配成功';				}
+					rply.text='配置成功';				}
 				else if(select=='放出適性' && Characters[fd][14]>=Points){
 					Characters[fd][9]=parseInt(Characters[fd][9],10)+parseInt(Points, 10);
 					Characters[fd][14]-=Points;
-					rply.text='分配成功';				}
+					rply.text='配置成功';				}
 				else if(select=='火屬適性' && Characters[fd][14]>=Points){
 					Characters[fd][10]=parseInt(Characters[fd][10],10)+parseInt(Points, 10);
 					Characters[fd][14]-=Points;
-					rply.text='分配成功';				}
+					rply.text='配置成功';				}
 				else if(select=='水屬適性' && Characters[fd][14]>=Points){
 					Characters[fd][11]=parseInt(Characters[fd][11],10)+parseInt(Points, 10);
 					Characters[fd][14]-=Points;
-					rply.text='分配成功';				}
+					rply.text='配置成功';				}
 				else if(select=='風屬適性' && Characters[fd][14]>=Points){
 					Characters[fd][12]=parseInt(Characters[fd][12],10)+parseInt(Points, 10);
 					Characters[fd][14]-=Points;
-					rply.text='分配成功';				}
+					rply.text='配置成功';				}
 				else if(select=='土屬適性' && Characters[fd][14]>=Points){
 					Characters[fd][13]=parseInt(Characters[fd][13],10)+parseInt(Points, 10);
 					Characters[fd][14]-=Points;
-					rply.text='分配成功';				}
+					rply.text='配置成功';				}
 				else{rply.text='點數不足';}
 				fs.readFile('client_secret.json', function processClientSecrets(err, content) {
   				if (err) {
