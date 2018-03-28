dd61a1bba565986e4a5449e2d5de7ec77a965314
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-28 21:07:46 +0800

    Update battle.js

--- roll/battle.js
+++ roll/battle.js
@@ -1,8 +1,8 @@
-/*var rollbase = require('./rollbase.js');
+var rollbase = require('./rollbase.js');
 var funny = require('./funny.js');
 var ox = require('./Character.js');
 var rply ={type : 'text'}; //type是必需的,但可以更改
-var player[];
+var player=[];
 
 function IDCA(id,name) {
 rply.text=name+'為測試人員';//U7c4779fd913aff927f26d7f6bedd87d1  雷洛Uc9b4571605aabd3e94edd7c189144278屬
@@ -14,13 +14,23 @@ return rply;
 function battles(id,name,ab) {
 	var ggg,ttt;
 if(ab='戰鬥參與'){
-var od[];
+var od=[];
   for(var i=0;i<ox.oL();i++){
 	if(ox.oC(i,0)==id){
 		ggg=i;
 		od[0]=ox.oC(i,0);
-    od[1]=ox.oC(i,1);
-    od[2]=ox.oC(i,2);
+    		od[1]=ox.oC(i,1);
+    		od[2]=ox.oC(i,5);
+		od[3]=ox.oC(i,5);
+		od[4]=ox.oC(i,6);
+		od[5]=ox.oC(i,6);
+		od[6]=ox.oC(i,7);
+		od[7]=ox.oC(i,8);
+		rply.text+=name+'你的'+od[1]+'已參與\n'+
+			'HP '+od[2]+'/'+od[3]+
+			'\nbata粒子 '+od[4]+'/'+od[5]+
+			'\n物理適性 '+od[6]+
+			'\n反應力'+od[7];
   }
 	}
 }
@@ -38,4 +48,4 @@ module.exports = {
 	battles:battles
 };
 
-*/
+
