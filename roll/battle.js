612cbc49486f7e425c86167874da252bb2f8d476
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-28 22:43:46 +0800

    Update battle.js

--- roll/battle.js
+++ roll/battle.js
@@ -4,6 +4,7 @@ var ox = require('./Character.js');
 var rply ={type : 'text'}; //type是必需的,但可以更改
 var player=[];
 var start=0;
+var hit=1;
 function IDCA(id,name) {
 rply.text=name+'為測試人員';//U7c4779fd913aff927f26d7f6bedd87d1  雷洛Uc9b4571605aabd3e94edd7c189144278屬
 if(id=='U7c4779fd913aff927f26d7f6bedd87d1')rply.text=name+'為GM';
@@ -49,7 +50,30 @@ var od=[];
 }
 if(ab=='戰鬥開始' && start==0 && player.length==2){
 		start=1;
-		rply.text='戰鬥展開';
+		
+		if(player[0][7]>=player[1][7])hit=0;
+	        rply.text='戰鬥展開'+
+			'\n'+player[hit][1]+'先手'+
+			'\n 可用選項：攻擊';
+		return rply;
+	}
+if(start==1){
+	if(id==player[hit][0] && ab='攻擊'){
+		player[1-hit][2]=player[1-hit][2]-player[hit][6];
+		hit=1-hit;
+	}
+	if(player[hit][2]<=0){
+		rply.text=player[1-hit][1]+'勝利';
+		start=0;
+		dd();
+		return rply;
+	}
+	rply.text=player[hit][1]+
+			'\nHP '+player[hit][2]+'/'+player[hit][3]+
+			'\nbata粒子 '+player[hit][4]+'/'+player[hit][5]+
+			'\n物理適性 '+player[hit][6]+
+			'\n反應力'+player[hit][7]+
+		'\n輪到'+player[hit][1]+'的回合了';
 		return rply;
 	}
 }
