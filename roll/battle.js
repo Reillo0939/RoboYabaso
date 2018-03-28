d71c81b28548038c252231680e64fd71713beadd
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-28 20:46:16 +0800

    Update battle.js

--- roll/battle.js
+++ roll/battle.js
@@ -1,1 +1,40 @@
+var rollbase = require('./rollbase.js');
+var funny = require('./funny.js');
+var ox = require('./Character.js');
+var rply ={type : 'text'}; //type是必需的,但可以更改
+var player[];
+
+function IDCA(id,name) {
+rply.text=name+'為測試人員';//U7c4779fd913aff927f26d7f6bedd87d1  雷洛Uc9b4571605aabd3e94edd7c189144278屬
+if(id=='U7c4779fd913aff927f26d7f6bedd87d1')rply.text=name+'為GM';
+if(id=='Uc9b4571605aabd3e94edd7c189144278')rply.text=name+'為GM';
+return rply;
+}
+
+function battles(id,name,ab) {
+	var ggg,ttt;
+if(ab='戰鬥參與'){
+var od[];
+  for(var i=0;i<ox.oL();i++){
+	if(ox.oC(i,0)==id){
+		ggg=i;
+		od[0]=ox.oC(i,0);
+    od[1]=ox.oC(i,1);
+    od[2]=ox.oC(i,2);
+  }
+	}
+}
+  
+
+	
+
+//rply.text+='\n多送你一張：';
+//let rarity=rollbase.Dice(100);
+//ox.oA(ggg,ttt-frequency*100);
+return rply;
+}
+
+module.exports = {
+	battles:battles
+};
 
