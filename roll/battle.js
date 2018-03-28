b4a024872afdf7dc72abf9b4566d7c1350b71170
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-28 23:19:19 +0800

    Add files via upload

--- roll/battle.js
+++ roll/battle.js
@@ -60,6 +60,7 @@ if(ab=='戰鬥開始' && start==0 && player.length==2){
 if(start==1){
 	if(id==player[hit][0] && ab=='攻擊'){
 		var rnggg;
+		console.log(player[1-hit][7]+player[hit][7]);
 		rnggg=rollbase.Dice(100);
 		if(rnggg>(50+player[1-hit][7]-player[hit][7])){
 			player[1-hit][2]=player[1-hit][2]-player[hit][6];
