c1f29db786937dfd9d98fb97dcf8c5ae67e638fa
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-29 22:02:03 +0800

    Update battle.js

--- roll/battle.js
+++ roll/battle.js
@@ -43,9 +43,11 @@ if(trigger.match(/^戰鬥參與$/) != null && start==0){
 var od=[];
   for(var i=0;i<ox.oL();i++){
 	if(ox.oC(i,0)==id){
-		if(player.length==1 && ox.oC(i,0)==player[0][0]){
-			rply.text='無法重複參與';
-		return rply;
+		if(var j=0;j<player.length;j++){
+			if(player.length==1 && ox.oC(i,0)==player[j][0]){
+				rply.text='無法重複參與';
+			return rply;
+			}
 		}
 		ggg=i;
 		od[0]=ox.oC(i,0);
