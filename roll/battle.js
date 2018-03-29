2c74e8850d333a4a883c0ebe829ebf37ed5f23b7
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-03-29 21:39:34 +0800

    Update battle.js

--- roll/battle.js
+++ roll/battle.js
@@ -5,6 +5,7 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 var player=[];
 var start=0;
 var hit=1;
+var self=0;
 var rnggg;
 var mode;
 function IDCA(id,name) {
@@ -120,9 +121,111 @@ if(start==1){
 		return rply;
 		}
 	}
+	
+	if(mode==2){
+if(trigger.match(/^戰鬥參與$/) != null && start==0){
+	if(player.length==4){
+		rply.text='已達參與上限';
+		return rply;}
+var od=[];
+  for(var i=0;i<ox.oL();i++){
+	if(ox.oC(i,0)==id){
+		if(player.length==1 && ox.oC(i,0)==player[0][0]){
+			rply.text='無法重複參與';
+		return rply;
+		}
+		ggg=i;
+		od[0]=ox.oC(i,0);
+    		od[1]=ox.oC(i,1);
+    		od[2]=ox.oC(i,5);
+		od[3]=ox.oC(i,5);
+		od[4]=ox.oC(i,6);
+		od[5]=ox.oC(i,6);
+		od[6]=ox.oC(i,7);
+		od[7]=ox.oC(i,8);
+		player[player.length]=od;
+		rply.text=name+'你的'+od[1]+'已參與\n'+
+			'HP '+od[2]+'/'+od[3]+
+			'\nbata粒子 '+od[4]+'/'+od[5]+
+			'\n物理適性 '+od[6]+
+			'\n反應力'+od[7];
+		return rply;
+  }
+	}
 }
-  
+		if(trigger.match(/^取消參與$/) != null&& start==0){
+			var od=[];
+			for(var i=0;i<player.length;i++){
+				if(player[i][0]==id){
+					od[1]=player[i][1];
+					player.splice(i,1);
+					rply.text=name+'你的'+od[1]+'已取消參與';
+					return rply; 
+				}
+			}
+			
+		}
+		if(trigger.match(/^戰鬥開始$/) != null && start==0 && player.length==4){
+			start=1;
+			self=0;
+			player.sort(function (a,b){return b[7]-a[7]});
+			rply.text='戰鬥展開'+
+			'\n'+player[self][1]+'先手'+
+			'\n 可用選項：攻擊';
+			return rply;
+		}
+		if(start==1){
+			if(id==player[self][0] && trigger.match(/^攻擊$/) != null && mainMsg[1] != null ){
+				for(var i=0;i<player.length;i++){
+					if(player[i][1]==mainMsg[1] && player[i][1]==player[self][1]){
+						rnggg=rollbase.Dice(100);
+						if(rnggg>(20+parseInt(player[i][7])-parseInt(player[self][7]))){
+							
+							player[i][2]=player[i][2]-player[self][6];
+							rply.text=player[i][1]+
+							'\nHP '+player[i][2]+'/'+player[i][3]+'(-'+player[self][6]+')'+
+							'\nbata粒子 '+player[i][4]+'/'+player[i][5]+
+							'\n物理適性 '+player[i][6]+
+							'\n反應力'+player[i][7];
+							self++;
+							if(self==player.length)self=0;
+							rply.text=rply.text+'\n\n輪到'+player[self][1]+'的回合了'+
+							'\n 可用選項：攻擊';
 
+							if(player.length==1){
+								rply.text= player[0][1]+'勝利';
+								start=0;
+								dd();
+								return rply;
+							}
+							return rply;
+						}
+						else{
+							rnggg=rollbase.Dice(100);
+							if(rnggg>(20+parseInt(player[i][7])-parseInt(player[self][7]))){
+							player[i][2]=player[i][2]-player[self][6];
+							self++;
+							if(self==4)self=0;
+							rply.text=player[i][1]+'閃避成功'+
+							'\nHP '+player[i][2]+'/'+player[i][3]+
+							'\nbata粒子 '+player[i][4]+'/'+player[i][5]+
+							'\n物理適性 '+player[i][6]+
+							'\n反應力'+player[i][7]+
+							'\n\n輪到'+player[self][1]+'的回合了'+
+							'\n 可用選項：攻擊';
+							return rply;
+							}
+						}
+					}
+			
+			rply.text=player[self][1]+'的回合'+
+				'\n 可用選項：攻擊';
+				return rply;
+				}
+			}
+		}
+	}
+}
 	
 
 //rply.text+='\n多送你一張：';
