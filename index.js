44bd90f32f47bbf6e0882f4c665e90c5bc48b949
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-04-01 21:59:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -38,27 +38,27 @@ b=profile.displayName;
 //Ca8fea1f8ef1ef2519860ee21fb740fd2   群id
 if(battle==1){
 if(event.message.text=='戰鬥模式關閉'){
-if(a=='U7c4779fd913aff927f26d7f6bedd87d1'||a=='Uc9b4571605aabd3e94edd7c189144278'){
+//if(a=='U7c4779fd913aff927f26d7f6bedd87d1'||a=='Uc9b4571605aabd3e94edd7c189144278'){
 battle=0;
 event.reply({ type: 'text', text: '已關閉戰鬥模式' });	
-}
-else{
-event.reply({type: 'text', text: 'GM才能使用' });	
-}
+//}
+//else{
+//event.reply({type: 'text', text: 'GM才能使用' });	
+//}
 }
 msg=ba.battles(a,b,event.message.text);
 event.reply(msg);
 }
 if(battle==0){
 if(event.message.text=='戰鬥模式啟動'){
-if(a=='U7c4779fd913aff927f26d7f6bedd87d1'||a=='Uc9b4571605aabd3e94edd7c189144278'){
+//if(a=='U7c4779fd913aff927f26d7f6bedd87d1'||a=='Uc9b4571605aabd3e94edd7c189144278'){
 battle=1;
 	ba.dd();
 event.reply({ type: 'text', text: '已啟動戰鬥模式' });	
-}
-else{
-event.reply({type: 'text', text: 'GM才能使用' });	
-}
+//}
+//else{
+//event.reply({type: 'text', text: 'GM才能使用' });	
+//}
 }
 		msg =exports.analytics.parseInput(event.rplyToken, event.message.text,a,b);
 if(event.message.text=='武裝裝甲聯合戰線'){
