52032c529180af3ec5f3da9efda2e01ffad3081f
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-04-02 20:37:33 +0800

    Update battle.js

--- roll/battle.js
+++ roll/battle.js
@@ -227,6 +227,213 @@ var od=[];
 		}
 			
 }
+
+
+
+function cb(aaab,mainMsg,trigger,id,name){
+if(trigger.match(/^戰鬥參與$/) != null && start==0){
+	if(player.length==aaab){
+		rply.text='已達參與上限';
+		return rply;}
+var od=[];
+  for(var i=0;i<ox.oL();i++){
+	if(ox.oC(i,0)==id){
+		for(var k=0;k<player.length;k++){
+		if(player.length>=1 && id==player[k][0]){
+			rply.text='無法重複參與';
+		return rply;
+		}
+		}
+		ggg=i;
+		od[0]=ox.oC(i,0);
+    	od[1]=ox.oC(i,1);
+    	od[2]=ox.oC(i,5);
+		od[3]=ox.oC(i,5);
+		od[4]=ox.oC(i,6);
+		od[5]=ox.oC(i,6);
+		od[6]=ox.oC(i,7);
+		od[7]=ox.oC(i,8);
+		od[8]=ox.oC(i,9);
+		od[9]=ox.oC(i,10);
+		od[10]=ox.oC(i,11);
+		od[11]=ox.oC(i,12);
+		od[12]=ox.oC(i,13);
+		od[13]=ox.oC(i,15);
+		od[14]=ox.oC(i,3);
+		od[15]=ox.oC(i,17);
+		//od[16]=ox.oC(i,18);
+		player[player.length]=od;
+		rply.text=name+'你的'+od[1]+'已參與\n'+
+			'HP '+od[2]+'/'+od[3]+
+			'\nbata粒子 '+od[4]+'/'+od[5]+
+			'\n物理適性 '+od[6]+
+			'\n反應力'+od[7]		
+			+'\n目前參與人數： '+player.length+'/'+aaab;
+		return rply;
+  }
+	}
+}
+		if(trigger.match(/^取消參與$/) != null&& start==0){
+			var od=[];
+			for(var i=0;i<player.length;i++){
+				if(player[i][0]==id){
+					od[1]=player[i][1];
+					player.splice(i,1);
+					rply.text=name+'你的'+od[1]+'已取消參與'		
+						+'\n目前參與人數： '+player.length+'/'+aaab;
+					return rply; 
+				}
+			}
+			
+		}
+		if(trigger.match(/^戰鬥開始$/) != null && start==0 && player.length==aaab){
+			start=1;
+			self=0;
+			player.sort(function (a,b){return b[7]-a[7]});
+			var ff=rollbase.Dice(4)-1;
+			for(var dasdsada=0;dasdsada<4;dasdsada++){
+				
+			}
+			rply.text='戰鬥展開'+
+			'\n'+player[self][1]+'先手'+
+			'\n 可用選項：攻擊 目標'+
+			'\n 目標有';
+			for(var k=0;k<player.length;k++){
+				if(player[k][1]!=player[self][1])rply.text=rply.text+'\n'+player[k][1];
+			}
+			return rply;
+		}
+		if(start==1){
+			if(id==player[self][0] && trigger.match(/^攻擊$/) != null && mainMsg[1] != null ){
+				for(var i=0;i<player.length;i++){
+					if(player[i][1]==mainMsg[1] ){
+						rnggg=rollbase.Dice(100);
+						if(rnggg > (20 + parseInt(player[i][7]) - parseInt(player[self][7]) ) ){
+							damage=Math.round(player[self][6]*(rollbase.Dice(10)+5)*0.1);
+							player[i][2]=player[i][2]-damage;
+							rply.text=player[i][1]+
+							'\nHP '+player[i][2]+'/'+player[i][3]+'(-'+damage+')'+
+							'\nbata粒子 '+player[i][4]+'/'+player[i][5];
+							self++;
+							if(player[i][2]<=0){
+								if(i<self)self--;
+								rply.text=rply.text+'\n'+player[i][1]+'已倒地';
+								
+								player.splice(i,1);
+							}
+							
+							if(player.length==1){
+								rply.text+='\n'+ player[0][1]+'勝利';
+								start=0;
+								dd();
+								return rply;
+							}
+							if(self>=player.length)self=0;
+							rply.text=rply.text+'\n\n輪到'+player[self][1]+'的回合了'+
+							'\n 可用選項：攻擊 目標'+
+							'\n 目標有';
+							for(var k=0;k<player.length;k++){
+								rply.text=rply.text+'\n'+player[k][1];
+							}
+							
+							return rply;
+						}
+						else{
+							self++;
+							if(self>=player.length)self=0;
+							rply.text=player[i][1]+'閃避成功'+
+							'\nHP '+player[i][2]+'/'+player[i][3]+
+							'\nbata粒子 '+player[i][4]+'/'+player[i][5]+
+							'\n\n輪到'+player[self][1]+'的回合了'+
+							'\n 可用選項：攻擊 目標'+
+							'\n 目標有';
+							for(var k=0;k<player.length;k++){
+								rply.text=rply.text+'\n'+player[k][1];
+							}
+							return rply;
+						}
+					}
+				}
+			}
+			if(trigger.match(/^跳過/) != null && start==1){
+					self++;
+					if(self>=player.length)self=0;
+					rply.text='輪到'+player[self][1]+'的回合了'+
+							'\n 可用選項：攻擊 目標'+
+							'\n 目標有';
+				for(var k=0;k<player.length;k++){
+					if(player[k][1]!=player[self][1])rply.text=rply.text+'\n'+player[k][1];
+				}
+				return rply;
+		}
+		if(trigger.match(/^我命令你死去/) != null && start==1){
+								player[self][2]=0;
+								rply.text=player[self][1]+'已倒地';
+								player.splice(self,1);
+							if(player.length==1){
+								rply.text+='\n'+ player[0][1]+'勝利';
+								start=0;
+								dd();
+								return rply;
+							}
+							self++;
+					if(self>=player.length)self=0;
+					rply.text='輪到'+player[self][1]+'的回合了'+
+							'\n 可用選項：攻擊 目標'+
+							'\n 目標有';
+				for(var k=0;k<player.length;k++){
+					if(player[k][1]!=player[self][1])rply.text=rply.text+'\n'+player[k][1];
+				}
+				return rply;
+		}
+			if(trigger.match(/^m/) != null){
+			}
+			else{
+			rply.text='輪到'+player[self][1]+'的回合了'+
+							'\n 可用選項：攻擊 目標'+
+							'\n 目標有';
+				for(var k=0;k<player.length;k++){
+					if(player[k][1]!=player[self][1])rply.text=rply.text+'\n'+player[k][1];
+				}
+				return rply;
+			}
+				
+		}
+			
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function boss01(aaab,mainMsg,trigger,id,name){
 if(trigger.match(/^戰鬥參與$/) != null && start==0){
 	if(player.length==aaab){
