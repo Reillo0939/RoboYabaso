ed54b4e9c948641a9980f9d130d29d400bc3681b
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-04-02 20:46:50 +0800

    Update battle.js

--- roll/battle.js
+++ roll/battle.js
@@ -10,12 +10,6 @@ var self=0;
 var damage;
 var rnggg;
 var mode;
-function IDCA(id,name) {
-rply.text=name+'為測試人員';//U7c4779fd913aff927f26d7f6bedd87d1  雷洛Uc9b4571605aabd3e94edd7c189144278屬
-if(id=='U7c4779fd913aff927f26d7f6bedd87d1')rply.text=name+'為GM';
-if(id=='Uc9b4571605aabd3e94edd7c189144278')rply.text=name+'為GM';
-return rply;
-}
 function dd() {
 player.length=0;
 }
@@ -24,7 +18,7 @@ function battles(id,name,ab) {
 	let msgSplitor = (/\S+/ig);	
 	let mainMsg = ab.match(msgSplitor); //定義輸入字串
 	let trigger = mainMsg[0].toString()
-	
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
 	if(trigger.match(/^2人對戰模式$/) != null && start==0){
 		mode=1;
 		dd();
@@ -49,12 +43,18 @@ function battles(id,name,ab) {
 	        rply.text='已轉為討伐愚人節boss模式(8人)';
 		return rply;
 	}
+	if(trigger.match(/^4人測試/) != null && start==0){
+		mode=99;
+		dd();
+	        rply.text='已轉為討伐愚人節boss模式(8人)';
+		return rply;
+	}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
 	rply.text='';
 	if(mode==1){
 		ga(2,mainMsg,trigger,id,name);
 		return rply;
 	}
-//------------------------------------------------------------------------
 	if(mode==2){
 ga(4,mainMsg,trigger,id,name);
 		return rply;
@@ -67,8 +67,12 @@ ga(4,mainMsg,trigger,id,name);
 			boss01(8,mainMsg,trigger,id,name);
 		return rply;
 		}
+		if(mode==99){
+			cb(4,mainMsg,trigger,id,name);
+		return rply;
+		}
 }
-	
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
 function ga(aaab,mainMsg,trigger,id,name){
 if(trigger.match(/^戰鬥參與$/) != null && start==0){
 	if(player.length==aaab){
@@ -227,9 +231,7 @@ var od=[];
 		}
 			
 }
-
-
-
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
 function cb(aaab,mainMsg,trigger,id,name){
 if(trigger.match(/^戰鬥參與$/) != null && start==0){
 	if(player.length==aaab){
@@ -245,23 +247,24 @@ var od=[];
 		}
 		}
 		ggg=i;
-		od[0]=ox.oC(i,0);
-    	od[1]=ox.oC(i,1);
-    	od[2]=ox.oC(i,5);
-		od[3]=ox.oC(i,5);
-		od[4]=ox.oC(i,6);
-		od[5]=ox.oC(i,6);
-		od[6]=ox.oC(i,7);
-		od[7]=ox.oC(i,8);
-		od[8]=ox.oC(i,9);
-		od[9]=ox.oC(i,10);
-		od[10]=ox.oC(i,11);
-		od[11]=ox.oC(i,12);
-		od[12]=ox.oC(i,13);
-		od[13]=ox.oC(i,15);
-		od[14]=ox.oC(i,3);
-		od[15]=ox.oC(i,17);
-		//od[16]=ox.oC(i,18);
+		od[0]=ox.oC(i,0);//ID
+    	od[1]=ox.oC(i,1);//名字
+    	od[2]=ox.oC(i,5);//生命值
+		od[3]=ox.oC(i,5);//生命值
+		od[4]=ox.oC(i,6);//Bata粒子適性
+		od[5]=ox.oC(i,6);//Bata粒子適性
+		od[6]=ox.oC(i,7);//物理適性
+		od[7]=ox.oC(i,8);//反應力
+		od[8]=ox.oC(i,9);//放出適性
+		od[9]=ox.oC(i,10);//火屬適性
+		od[10]=ox.oC(i,11);//水屬適性
+		od[11]=ox.oC(i,12);//風屬適性
+		od[12]=ox.oC(i,13);//土屬適性
+		od[13]=ox.oC(i,15);//控制能力
+		od[14]=ox.oC(i,3);//陣營
+		od[15]=ox.oC(i,17);//金幣
+		od[16]=0;//x
+		od[17]=0;//y
 		player[player.length]=od;
 		rply.text=name+'你的'+od[1]+'已參與\n'+
 			'HP '+od[2]+'/'+od[3]+
@@ -291,12 +294,26 @@ var od=[];
 			self=0;
 			player.sort(function (a,b){return b[7]-a[7]});
 			var ff=rollbase.Dice(4)-1;
-			for(var dasdsada=0;dasdsada<4;dasdsada++){
-				
-			}
+				player[ff][16]=1;
+				player[ff][17]=1;
+				ff++;
+				if(ff>=4)ff=0;
+				player[ff][16]=10;
+				player[ff][17]=1;
+				ff++;
+				if(ff>=4)ff=0;
+				player[ff][16]=1;
+				player[ff][17]=10;
+				ff++;
+				if(ff>=4)ff=0;
+				player[ff][16]=10;
+				player[ff][17]=10;
 			rply.text='戰鬥展開'+
 			'\n'+player[self][1]+'先手'+
-			'\n 可用選項：攻擊 目標'+
+			'\n位置 '+player[self][16]+','+player[self][17];
+			'\n 可用選項：'+
+			'\n攻擊 目標'+
+			'\n移動 x座標,y座標'
 			'\n 目標有';
 			for(var k=0;k<player.length;k++){
 				if(player[k][1]!=player[self][1])rply.text=rply.text+'\n'+player[k][1];
@@ -401,39 +418,7 @@ var od=[];
 		}
 			
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
 function boss01(aaab,mainMsg,trigger,id,name){
 if(trigger.match(/^戰鬥參與$/) != null && start==0){
 	if(player.length==aaab){
