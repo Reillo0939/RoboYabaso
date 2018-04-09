28377536abd40afdf19141b0819679edf0fdf4f2
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-04-09 21:07:50 +0800

    Add files via upload

--- roll/battle.js
+++ roll/battle.js
@@ -365,6 +365,7 @@ var od=[];
 			return rply;
 		}
 		if(start==1){
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
 			if(id==player[self][0] && trigger.match(/^攻擊$/) != null && mainMsg[1] != null &&player[self][18]>=9 && player[self][18]<=11 ){
 				for(var i=0;i<player.length;i++){
 					if(player[i][1]==mainMsg[1] ){
@@ -411,9 +412,9 @@ var od=[];
 							player[i][2]=player[i][2]-damage;
 							
 							rply.text+='(-'+damage+')';
-							if(Critical>=65 && player[self][18]==9)rply.text+='Critical'
-							if(Critical>=55 && player[self][18]==10)rply.text+='Critical'
-							if(Critical>=45 && player[self][18]==11)rply.text+='Critical'
+							if(Critical>=65 && player[self][18]==9)rply.text+='Critical';
+							if(Critical>=55 && player[self][18]==10)rply.text+='Critical';
+							if(Critical>=45 && player[self][18]==11)rply.text+='Critical';
 							rply.text+='\nbata粒子 '+player[i][4]+'/'+player[i][5];
 							ds++
 							if(ds==3){self++;ds=1;}
@@ -447,7 +448,111 @@ var od=[];
 					}
 				}
 			}
-			
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+			if(id==player[self][0] && trigger.match(/^單發射擊$/) != null && mainMsg[1] != null &&player[self][18]>=1 && player[self][18]<=5 && player[self][20]>0){
+				for(var i=0;i<player.length;i++){
+					if(player[i][1]==mainMsg[1] ){
+						var temp =0;
+						temp = Math.ceil((Math.pow(Math.pow(player[i][16]-player[self][16],2)+Math.pow(player[i][17]-player[self][17],2),0.5)));
+						if(temp>player[self][24]){
+							rply.text='距離'+player[i][1]+'太遠，無法攻擊';
+							return rply;
+						}
+						rnggg=rollbase.Dice(100);
+						Hit=rollbase.Dice(100);
+						Critical=rollbase.Dice(100);
+						if(Hit>80 && player[self][18]==1){
+							ds++;
+							if(ds==3){self++;ds=1;}
+							if(self>=player.length)self=0;
+							rply.text=player[i][1]+'沒有命中'+
+							'\n\n'+BR();
+			return rply;
+						}
+						if(Hit>80 && player[self][18]==2){
+							ds++;
+							if(ds==3){self++;ds=1;}
+							if(self>=player.length)self=0;
+							rply.text=player[i][1]+'沒有命中'+
+							'\n\n'+BR();
+			return rply;
+						}
+							if(Hit>40 && player[self][18]==3){
+								ds++;
+							if(ds==3){self++;ds=1;}
+							if(self>=player.length)self=0;
+							rply.text=player[i][1]+'沒有命中'+
+							'\n\n'+BR();
+			return rply;
+							}
+		  if(Hit>50 && player[self][18]==4){
+								ds++;
+							if(ds==3){self++;ds=1;}
+							if(self>=player.length)self=0;
+							rply.text=player[i][1]+'沒有命中'+
+							'\n\n'+BR();
+			return rply;
+							}
+			if(Hit>60 && player[self][18]==5){
+								ds++;
+							if(ds==3){self++;ds=1;}
+							if(self>=player.length)self=0;
+							rply.text=player[i][1]+'沒有命中'+
+							'\n\n'+BR();
+			return rply;
+							}
+						if(rnggg > (20 + parseInt(player[i][7]) - parseInt(player[self][7]) ) ){
+							rply.text=player[i][1]+
+							'\nHP '+player[i][2]+'/'+player[i][3];
+							damage=Math.round(player[self][19]*player[self][22]*(rollbase.Dice(10)+5)*0.1);
+							if(Critical>=50 && player[self][18]==1)damage=damage*1.5;
+							if(Critical>=60 && player[self][18]==2)damage=damage*1.5;
+							if(Critical>=20 && player[self][18]==3)damage=damage*1.5;
+							if(Critical>=30 && player[self][18]==4)damage=damage*1.5;
+							if(Critical>=40 && player[self][18]==5)damage=damage*1.5;
+							player[i][2]=player[i][2]-damage;
+							
+							rply.text+='(-'+damage+')';
+							if(Critical>=50 && player[self][18]==9)rply.text+='Critical';
+							if(Critical>=60 && player[self][18]==10)rply.text+='Critical';
+							if(Critical>=20 && player[self][18]==11)rply.text+='Critical';
+							if(Critical>=30 && player[self][18]==10)rply.text+='Critical';
+							if(Critical>=40 && player[self][18]==11)rply.text+='Critical';
+							rply.text+='\nbata粒子 '+player[i][4]+'/'+player[i][5];
+							player[self][20]--;
+							ds++
+							if(ds==3){self++;ds=1;}
+							if(player[i][2]<=0){
+								if(i<self)self--;
+								rply.text=rply.text+'\n'+player[i][1]+'已倒地';
+								
+								player.splice(i,1);
+							}
+							
+							if(player.length==1){
+								rply.text+='\n'+ player[0][1]+'勝利';
+								start=0;
+								dd();
+								return rply;
+							}
+							if(self>=player.length)self=0;
+							rply.text=rply.text+'\n\n'+BR();
+			return rply;
+						}
+						else{
+						ds++;
+							if(ds==3){self++;ds=1;}
+							if(self>=player.length)self=0;
+							rply.text=player[i][1]+'閃避成功'+
+							'\nHP '+player[i][2]+'/'+player[i][3]+
+							'\nbata粒子 '+player[i][4]+'/'+player[i][5]+
+							'\n\n'+BR();
+			return rply;
+						}
+					}
+				}
+			}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
 			if(id==player[self][0] && trigger.match(/^裝填子彈$/) != null && player[self][18]>=1 && player[self][18]<=8 && player[self][20]!=player[self][21]){
 					player[self][20]=player[self][21];
 					ds++
@@ -456,10 +561,7 @@ var od=[];
 					rply.text+='\n\n'+BR();
 			return rply;
 			}
-			
-			
-			
-			
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
 			if(id==player[self][0] && trigger.match(/^移動/) != null && start==1 &&  mainMsg[1] != null){
 				let xxyy = mainMsg[1].split(','); //定義輸入字串
 				if(isNaN(xxyy[0])==0 && isNaN(xxyy[1])==0){
@@ -491,6 +593,7 @@ var od=[];
 					rply.text+='\n\n'+BR();
 			return rply;
 		}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
 			if(trigger.match(/^跳過/) != null && start==1){
 					self++;
 					ds=1;
