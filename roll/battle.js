fee5a169f9462b5d1e12f5d2957e54323f1941b3
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-04-14 19:55:35 +0800

    Add files via upload

--- roll/battle.js
+++ roll/battle.js
@@ -64,7 +64,7 @@ function battles(id,name,ab) {
 		return rply;
 	}
 if(trigger.match(/^2人陣營模式/) != null && start==0){
-		faf.tis('Ca8fea1f8ef1ef2519860ee21fb740fd2','test');
+		bot.push('Ca8fea1f8ef1ef2519860ee21fb740fd2','test');
 		mode=12;
 		dd();
 	        rply.text='已轉為2人陣營模式';
