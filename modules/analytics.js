2b8f87e1979f993a14dda6db1bce0c85b54193a7
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-06-08 14:27:34 +0800

    Add files via upload

--- modules/analytics.js
+++ modules/analytics.js
@@ -112,10 +112,10 @@ function parseInput(rplyToken, inputStr, id,name) {
 	if (trigger.match(/改造/)!= null) return exports.weapon.weapon_retrofit(id,name,mainMsg[1],mainMsg[2]) ;
 	
 	}
-	if (trigger.match(/技能/) != null){
+	/*if (trigger.match(/技能/) != null){
 	if (trigger.match(/製作/)!= null) return exports.skill.skill_make(id,name,mainMsg[1],mainMsg[2]) ;
 		if (trigger.match(/查看/)!= null) return exports.skill.skill_view(id,name,mainMsg[1]) ;
-	}
+	}*/
 	
 	if (trigger.match(/^角色創立說明$/) != null) return exports.help.Character() ;
 	
