991d60ef881e6f3748bbd50df5f848df4f4cae96
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-06-23 16:39:18 +0800

    Add files via upload

--- roll/card.js
+++ roll/card.js
@@ -14,226 +14,146 @@ return rply;
 function MCard(frequency,id,name) {
 	var ggg,ttt;
 	console.log(ox.oL());
-	for(var i=0;i<ox.oL();i++){
+	/*for(var i=0;i<ox.oL();i++){
+		
 	if(ox.oC(i,0)==id){
 		ggg=i;
 		ttt=ox.oC(ggg,17);}
-	}
+	}*/
 	/*if(ttt<frequency*100){
 		rply.text=name+'金錢不足：';
 		return rply;
 	}*/
 rply.text=name+'抽到了：';
-if(id==''){
+/*if(id==''){
 for(i=1;i<=frequency;i++){
  SSR();
 }
 }
-else{
+else{*/
 for(i=1;i<=frequency;i++){
-let rarity=rollbase.Dice(100);
-if(rarity>=51)MSSR();
-if((rarity>=21) && (rarity<=50))MSR();
- if((rarity>=11) && (rarity<=20))MR();
- if(rarity<=10)MN();
+let rarity=rollbase.Dice(1000);
+if(rarity>=1000)MUR();
+if((rarity>=990) && (rarity<=999))MSSR();
+ if((rarity>=891) && (rarity<=990))MSR();
+  if((rarity>=591) && (rarity<=890))MR();
+ if(rarity<=590)MN();
 }
 
-if(frequency==10){
-rply.text+='\n多送你一張：';
-let rarity=rollbase.Dice(100);
-if(rarity>=21)MSSR();
-if(rarity<=20)MSR();
+if(frequency==9){
+rply.text+='\n保底：';
+let rarity=rollbase.Dice(1000);
+if(rarity>=996)MUR();
+if((rarity>=966) && (rarity<=995))MSSR();
+if(rarity<=965)MSR();
 }
-}
+//}
 //ox.oA(ggg,ttt-frequency*100);
 return rply;
 }
 function MN() {
 let rplyArr = [
-'\[002] [N] [Item] 雷洛的程式', 
-'\[003] [N] [Item] 一定墜落的翅膀',
-'\[004] [N] [Item] 雷銘的筆',
-'\[006] [N] [Accessories] 火之庇護',
-'\[007] [N] [Accessories] 水之庇護',
-'\[008] [N] [Accessories] 風之庇護',
-'\[009] [N] [Accessories] 土之庇護',
-'\[010] [N] [Skill] 熱量控制',
-'\[011] [N] [Skill] 燃燒現象',
-'\[012] [N] [Skill] 質能轉換-水',
-'\[013] [N] [Skill] 水流控制',
-'\[014] [N] [Skill] 簡單氣壓控制',
-'\[015] [N] [Skill] 強制位移',
-'\[016] [N] [Skill] 密度控制-物',
-'\[017] [N] [Skill] 土牆建立',
-'\[021] [N] [Skill] 能量轉換概論',
-'\[022] [N] [Skill] 能量放出實驗'
+'\[N] 普通人類-冰夜',
+'\[N] 壓路機',
+'\[N] 飛刀',
+'\[N] 替身',
+'\[N] 替身使者',
+'\[N] 手槍',
+'\[N] 重型手槍',
+'\[N] 衝鋒槍',
+'\[N] 突擊步槍',
+'\[N] 射手步槍',
+'\[N] 狙擊槍',
+'\[N] 大口徑狙擊槍',
+'\[N] 火炮',
+'\[N] 短近距離武器',
+'\[N] 中近距離武器',
+'\[N] 長近距離武器',
+'\[N] 路邊的石頭'
 ];
 rply.text = rply.text +'\n'+ rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 return rply;	
 }
 function MR() {
 let rplyArr = [
-'\[030] [N] [Item] 偽裝成[R]的[N]卡',
-'\[032] [R] [Item] 雷洛那有點強的bot',
-'\[033] [R] [Item] 翅膀那雷神',
-'\[034] [R] [Item] 雷銘的0.2mm自動鉛筆',
-'\[036] [R] [Skill] 熱距離延長',
-'\[037] [R] [Skill] 燃燒現象強化',
-'\[038] [R] [Skill] 水壓提升(小)',
-'\[039] [R] [Skill] 造霧',
-'\[040] [R] [Skill] 噴氣移動',
-'\[041] [R] [Skill] 位移加速',
-'\[042] [R] [Skill] 沙漠化',
-'\[043] [R] [Skill] 流沙',
-'\[047] [R] [Skill] 能量壓縮實驗',
-'\[048] [R] [Skill] 簡單建構模擬'
+'\[R] 雷銘繪製的圖',
+'\[R] 夜月的程式片段',
+'\[R] 武器-暗殺者', 
+'\[R] 武器-反甲騎兵槍', 
+'\[R] 武器-FAL', 
+'\[R] 武器-m16a4', 
+'\[R] 武器-霰射炸藥', 
+'\[R] 武器-FrostGiant', 
+'\[R] 武器-我不知道能不能用', 
+'\[R] 武器-3HIT'
 ];
 rply.text = rply.text +'\n'+ rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 return rply;	
 }
 function MSR() {
 let rplyArr = [
-'\[060] [N] [Item] 偽裝成[SR]的[N]卡',
-'\[062] [SR] [Item] 雷洛那超簡潔的程式',
-'\[063] [SR] [Item] 翅膀雷好雷滿',
-'\[064] [SR] [Item] 雷銘的用不壞的平板',
-'\[066] [SR] [Skill] 燃燒標記',
-'\[067] [SR] [Skill] 創水控制',
-'\[068] [SR] [Skill] 風壓推移',
-'\[069] [SR] [Skill] 點石成金',
-'\[074] [SR] [Skill] 屬性纏繞'
+'\[SR] 雷洛的研究紀錄',
+'\[SR] 粒子儲能電池',
+'\[SR] 忙碌者-Wings',
+'\[SR] 加速插件',
+'\[SR] 狂暴插件',
+'\[SR] 繪師-雷銘',
+'\[SR] 鼠用來研究的筆電',
+'\[SR] 狙擊手-凜夢',
+'\[SR] CAC系統',
+'\[SR] 粒子觸媒',
+'\[SR] 測試人員-Guni',
+'\[SR] 測試人員-赤靈',
+'\[SR] 聖戰發起者-lee',
+'\[SR] 迷之表情-(´・ω・`)'
 ];
 rply.text = rply.text +'\n' + rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 return rply;	
 }
 function MSSR() {
 let rplyArr = [
-'\[090] [N] [Item] 偽裝成[SSR]的[N]卡',
-'\[091] [SSR] [Accessories] 颶風之神', 
-'\[092] [SSR] [Accessories] 烈火之神', 
-'\[093] [SSR] [Accessories] 大海之神',
-'\[094] [SSR] [Accessories] 大地之神',
-'\[096] [SSR] [Partner] 雷洛',
-'\[097] [SSR] [Partner] Wings',
-'\[098] [SSR] [Partner] 雷銘',
-'\[100] [SSR] [Skill] 萬物燃盡',
-'\[101] [SSR] [Skill] 水刃劃出',
-'\[102] [SSR] [Skill] 風壓炸裂',
-'\[103] [SSR] [Skill] 大地石封',
-'\[106] [SSR] [Skill] 具現化概論'
+'\[SSR] 過載插件'
+'\[SSR] 無人擊敗-愚人節boss',
+'\[SSR] β粒子研究報告',
+'\[SSR] 具現化研究者-洛斯塔克',
+'\[SSR] 雙生者-夜玥',
+'\[SSR] AAUF王牌-咲',
+'\[SSR] GU王牌-夢',
+'\[SSR] 協力者-謝諾亞',
+'\[SSR] 隔壁的bot-上原空音',
+'\[SSR] 悲劇之人-製杖',
+'\[SSR] 程式設計師-夜月'
 ];
 rply.text = rply.text +'\n'+ rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 return rply;	
 }
 
-function TCard(frequency,id,name) {
-	var ggg,ttt;
-	console.log(ox.oL());
-	for(var i=0;i<ox.oL();i++){
-	if(ox.oC(i,0)==id){
-		ggg=i;
-		ttt=ox.oC(ggg,17);}
-	}
-	/*if(ttt<frequency*100){
-		rply.text=name+'金錢不足：';
-		return rply;
-	}*/
-rply.text=name+'抽到了：';
-if(id==''){
-for(i=1;i<=frequency;i++){
- SSR();
-}
-}
-else{
-for(i=1;i<=frequency;i++){
-let rarity=rollbase.Dice(100);
-if(rarity>=51)TSSR();
-if((rarity>=21) && (rarity<=50))TSR();
- if((rarity>=11) && (rarity<=20))TR();
- if(rarity<=10)TN();
+function MUR() {
+let rplyArr = [
+'\[UR] β粒子研究者-雷洛',
+'\[UR] β粒子發現者-鼠'
+];
+rply.text = rply.text +'\n'+ rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
+return rply;	
 }
 
-if(frequency==10){
-rply.text+='\n多送你一張：';
-let rarity=rollbase.Dice(100);
-if(rarity>=21)TSSR();
-if(rarity<=20)TSR();
-}
-}
-	//ox.oA(ggg,ttt-frequency*100);
-return rply;
-}
-
-function TN() {
-let rplyArr = [
-'\[001] [N] [Item] 鼠的畫', 
-'\[005] [N] [Item] 夜月的肝',
-'\[018] [N] [Skill] 磁懸推進',
-'\[019] [N] [Skill] 連結武裝切換',
-'\[020] [N] [Skill] 複合武器切換'
-];
-rply.text = rply.text +'\n'+ rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
-return rply;	
-}
-function TR() {
-let rplyArr = [
-'\[030] [N] [Item] 偽裝成[R]的[N]卡',
-'\[031] [R] [Item] 鼠的畫ex',
-'\[035] [R] [Item] 夜月那快撐不住的肝',
-'\[044] [R] [Skill] 墊步突擊',
-'\[045] [R] [Skill] 誤差修正',
-'\[046] [R] [Skill] 雷達'
-];
-rply.text = rply.text +'\n'+ rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
-return rply;	
-}
-function TSR() {
-let rplyArr = [
-'\[060] [N] [Item] 偽裝成[SR]的[N]卡',
-'\[061] [SR] [Item] 鼠那超進步的畫',
-'\[065] [SR] [Item] 夜月那超越人類的肝',
-'\[070] [SR] [Skill] 全彈射擊',
-'\[071] [SR] [Skill] 極速穿擊',
-'\[072] [SR] [Skill] 具現化武裝:76mm炮',
-'\[073] [SR] [Skill] 具現化武裝:八連裝導彈倉'
-];
-rply.text = rply.text +'\n' + rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
-return rply;	
-}
-function TSSR() {
-let rplyArr = [
-'\[090] [N] [Item] 偽裝成[SSR]的[N]卡',
-'\[095] [SSR] [Partner] 鼠',
-'\[099] [SSR] [Partner] 夜月',
-'\[104] [SSR] [Skill] 具現化武裝:電磁軌加速炮',
-'\[105] [SSR] [Skill] 具現化武裝:浮游護盾'
-];
-rply.text = rply.text +'\n'+ rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
-return rply;	
-}
 function ICard() {
 rply.text = 
 	'抽卡方式 \n'+
-	'單抽：卡池名稱單抽\n'+
-	'十連抽：卡池名稱10連抽\n'+
+	'單抽："卡池名稱"單抽\n'+
+	'十連抽："卡池名稱"10連抽\n'+
 	'本周卡池\n'+
-	'===GU:===\n'+
-	' -[SSR]機率50% \n'+
-	' -[SR]機率30% \n'+
-	' -[R]機率10% \n'+
-	' -[N]機率10% \n'+
+	'===粒子研究紀錄===\n'+
+	' -[UR]機率0.1% \n'+
+	' -[SSR]機率1% \n'+
+	' -[SR]機率10% \n'+
+	' -[R]機率30% \n'+
+	' -[N]機率58.9% \n'+
 	'===10連抽保底SR機率：===\n'+
-	' -[SSR]機率80% \n'+
-	' -[SR]機率20% \n'+
-	'--------------------------\n'+
-	'===AAUF:===\n'+
-	' -[SSR]機率50% \n'+
-	' -[SR]機率30% \n'+
-	' -[R]機率10% \n'+
-	' -[N]機率10% \n'+
-	'===10連抽保底SR機率：===\n'+
-	' -[SSR]機率80% \n'+
-	' -[SR]機率20%'
+	' -[UR]機率0.5% \n'+
+	' -[SSR]機率3% \n'+
+	' -[SR]機率96.5% \n'
+	
 	;
 return rply;	
 }
@@ -243,7 +163,6 @@ return rply;
 
 module.exports = {
 	MCard:MCard,
-	TCard:TCard,
 	ICard:ICard,
 	IDCA:IDCA
 };
