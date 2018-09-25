4ab10a338258d02825ae476c2af6ade9bd21b332
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-09-25 16:24:20 +0800

    Update help.js

--- roll/help.js
+++ roll/help.js
@@ -3,7 +3,7 @@ var funny = require('./funny.js');
 var rply ={type : 'text'}; //type是必需的,但可以更改
 
 function Help() {
-rply.text ='【夢之領域BOT】v0.9.7.1' +
+rply.text ='【夢之領域BOT】v0.9.7.1.' +
 '\n 卡池資訊'+
 '\n  -查看本周卡池的資訊與抽卡方式'+
 '\n 卡片查詢 卡片id  '+
@@ -15,7 +15,7 @@ rply.text ='【夢之領域BOT】v0.9.7.1' +
 return rply;	
 }
 function Character() {
-rply.text ='【角色創立系統】v1.5.?' +
+rply.text ='【角色創立系統】v1.5.1.?' +
 '\n GU角色創立 名稱(不能有空格) 年齡 '+
 '\n  -創立一個陣營為GU的角色  '+
 '\n AAUF角色創立 名稱(不能有空格) 年齡 '+
@@ -25,8 +25,10 @@ rply.text ='【角色創立系統】v1.5.?' +
 '\n  -歸0時無法繼續戰鬥  '+
 '\n CE儲存量  '+
 '\n  -使用技能會消耗'+
-'\n 耐重量  '+
-'\n  -跟可以拿的武器、裝備等有關'+
+'\n 射擊能力  '+
+'\n  -跟使用遠距離武器有關'+
+'\n 格鬥能力  '+
+'\n  -跟使用近距離武器有關'+
 '\n 反應力'+
 '\n  -與攻擊優先度等有關';
 return rply;	
