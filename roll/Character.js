870d54072c3ec78864b3fe8f841ea4f351dfd227
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2018-12-16 21:39:33 +0800

    Add files via upload

--- roll/Character.js
+++ roll/Character.js
@@ -26,14 +26,10 @@ client.connect(function(err) {
 });
 const insertDocuments = function(db, callback) {
   // Get the documents collection
-  const collection = db.collection('documents');
+  const collection = db.collection('player');
   // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
+  collection.insertMany(player, function(err, result) {
     assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
     console.log("Inserted 3 documents into the collection");
     callback(result);
   });
