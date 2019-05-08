fe68482afa41d22005b6610b0d9be66a04d2f29b
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2019-05-08 10:42:19 +0800

    Update card.js

--- roll/card.js
+++ roll/card.js
@@ -36,11 +36,6 @@ function MCard(frequency,id,name) {
 }
 function MN() {
 	let rplyArr = [
-		'\[N] 普通人類-冰夜',
-		'\[N] 壓路機',
-		'\[N] 飛刀',
-		'\[N] 替身',
-		'\[N] 替身使者',
 		'\[N] 手槍',
 		'\[N] 步槍',
 		'\[N] 拳',
@@ -48,16 +43,13 @@ function MN() {
 		'\[N] 大口徑狙擊槍',
 		'\[N] 刀',
 		'\[N] 長槍',
-		'\[N] 盾',
-		'\[N] 路邊的石頭'
+		'\[N] 盾'
 	];
 	rply.text = rply.text +'\n'+ rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 	return rply;	
 }
 function MR() {
 	let rplyArr = [
-		'\[R] 雷銘繪製的圖',
-		'\[R] 夜月的程式片段',
 		'\[R] 武器-暗殺者', 
 		'\[R] 武器-反甲騎兵槍', 
 		'\[R] 武器-FAL', 
@@ -72,52 +64,18 @@ function MR() {
 	return rply;	
 }
 function MSR() {
-	let rplyArr = [	
-		'\[SR] スサノオ',
-		'\[SR] イージス',
-		'\[SR] アイオーン',
-		'\[SR] ケリュケイオン',
-		'\[SR] ムラサメ',	       
-		'\[SR] 雷洛的研究紀錄',	       
-		'\[SR] 雷洛的研究紀錄',
+	let rplyArr = [	 
 		'\[SR] 粒子儲能電池',
-		'\[SR] 忙碌者-Wings',
 		'\[SR] 加速插件',
 		'\[SR] 狂暴插件',
-		'\[SR] 繪師-雷銘',
-		'\[SR] 鼠用來研究的筆電',
-		'\[SR] 狙擊手-凜夢',
 		'\[SR] CAC系統',
-		'\[SR] 粒子觸媒',
-		'\[SR] 測試人員-Guni',
-		'\[SR] 測試人員-赤靈',
-		'\[SR] 聖戰發起者-lee',
-		'\[SR] 迷之表情-(´・ω・`)', 
-		'\[SR] 來自異界的魔龍眼少年路卡', 
-		'\[SR] 迷之少年-Arthur'
+		'\[SR] 粒子觸媒'
 	];
 	rply.text = rply.text +'\n' + rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 return rply;	
 }
 function MSSR() {
     let rplyArr = [
-    '\[SSR] 神剣グラム',
-    '\[SSR] 覇剣デュランダル',
-    '\[SSR] 霊刀イザナミ',
-    '\[SSR] 滅剣・ベリアル',		   
-    '\[SSR] 神槍グングニル',		   
-    '\[SSR] 神槍グングニル',			    
-    '\[SSR] 古代機バルバトス',
-    '\[SSR] 過載插件',
-    '\[SSR] 無人擊敗-愚人節boss',
-    '\[SSR] β粒子研究報告',
-    '\[SSR] 具現化研究者-洛斯塔克',
-    '\[SSR] 雙生者-夜玥',
-    '\[SSR] AAUF王牌-咲',
-    '\[SSR] GU王牌-夢',
-    '\[SSR] 協力者-謝諾亞',
-    '\[SSR] 隔壁的bot-上原空音',
-    '\[SSR] 悲劇之人-製杖',
     '\[SSR] 神器-Excalibur',
     '\[SSR] 神器-高周波刀',
     '\[SSR] 妖刀-村正',
@@ -125,9 +83,7 @@ function MSSR() {
     '\[SSR] 神劍-莫邪',
     '\[SSR] 神劍-干將',
     '\[SSR] 魔劍-提爾鋒',
-    '\[SSR] 神槍-岡格尼爾',
-    '\[SSR] 程式設計師-夜月',
-    '\[SSR] 獸娘蘿莉充氣娃娃(?'
+    '\[SSR] 神槍-岡格尼爾'
     ];
 	rply.text = rply.text +'\n'+ rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 	return rply;	
@@ -135,10 +91,6 @@ function MSSR() {
 
 function MUR() {
 	let rplyArr = [
-		'\[UR] 覇剣イフリート',
-		'\[UR] 霊刀セイレーン',
-		'\[UR] 課金獎勵-普利珠',
-		'\[UR] https://www.youtube.com/watch?v=h-mUGj41hWA&index=18&list=PLP3hF47qMk4JiEo8SHISpQc9sePDbzLFm&t=0s',
 		'\[UR] 賢者之石',
 		'\[UR] 水晶研究者-雷洛',
 		'\[UR] 研究主導者-鼠'
