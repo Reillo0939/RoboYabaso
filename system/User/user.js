f2044ce9a65b4d623d288070b61cc71f9542f046
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2019-10-17 01:58:40 +0800

    Update user.js

--- system/User/user.js
+++ system/User/user.js
@@ -26,16 +26,12 @@ function create_User(UserId,UserName,Message,replyToken){
 			Mongoclient.db(dbName).collection('user').findOne({UserId:UserId}).then((data)=> {
 			if(data!=null){
 				rply.text=UserName+" 帳號已存在";
-				bot.reply(replyToken, rply);
-				return rply;
 			}
 			else{
 				let mainMsg = Message.match(msgSplitor);
 				let NickName=mainMsg[1];
 				if(NickName==null||NickName==undefined){
 					rply.text=UserName+" 缺少暱稱";
-					bot.reply(replyToken, rply);
-					return rply;
 				}
 				Mongoclient.connect(function(err) {
 					assert.equal(null, err);
@@ -44,9 +40,8 @@ function create_User(UserId,UserName,Message,replyToken){
 					});
 				});
 				rply.text=UserName+" / "+NickName+" 帳號已創建完畢";
-				bot.reply(replyToken, rply);
-				return rply;
 			}
+			bot.reply(replyToken, rply);
 		});
 	});
 	return -1;
@@ -59,14 +54,14 @@ function Inquire_User(UserId,UserName,Message,replyToken){
 		Mongoclient.db(dbName).collection('user').findOne({UserId:UserId}).then((data)=>{
 			if(data!=null){
 				rply.text=data.NickName+"\n擁有"+data.money+"G";
-				return rply;
 			}
 			else{
 				rply.text=UserName+" 沒有帳號喔";
-				return rply;
 			}
+			bot.reply(replyToken, rply);
 		});
 	});
+	return -1;
 }
 
 module.exports = {
