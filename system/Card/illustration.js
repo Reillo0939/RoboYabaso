b84d2b4fc1737337d0c62d64ba65c1ff9c129052
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2019-10-23 21:35:38 +0800

    Update illustration.js

--- system/Card/illustration.js
+++ system/Card/illustration.js
@@ -16,42 +16,44 @@ function illustration(UserId,UserName,Message,replyToken){
 	Mongoclient.connect(function(err) {
 		assert.equal(null, err);
 		//console.log("Connected successfully to server");
-		Mongoclient.db(dbName).collection('card').find({}).toArray().then((data)=> {
+		if(mainMsg[1]==undefined||mainMsg[1]==null||isNaN(mainMsg[1])){
+			Mongoclient.db(dbName).collection('card').find({}).toArray().then((data)=> {
 				console.log(data);
 				data.sort(function(a, b) {
 				  return a.ID - b.ID;
 				});
-				if(mainMsg[1]==undefined||mainMsg[1]==null||isNaN(mainMsg[1])){
-					rply.text="圖鑑";
+				rply.text="圖鑑";
 					for(let card of data){
 						rply.text+="\n["+card.ID+"]"+card.Name;
 					}
+				re_message.Line_reply(replyToken, rply);
+			});
+		}
+		else{
+			Mongoclient.db(dbName).collection('card').findOne({ID:mainMsg[1]}).then((data)=> {
+				if(data!=null){
+					rply.text="["+data.ID+"]"+data.Name+"\n"+
+								  "簡介:"+data.Introduction+"\n"+
+								  "HP/MP/AP/ATK:"+data.HP+"/"+data.MP+"/"+data.AP+"/"+data.ATK+"\n"+
+								  "移動消耗的AP/攻擊消耗的AP:"+data.MoveAP+"/"+data.AttackAP+"\n"+
+								  "攻擊距離:"+data.AttackRange+"\n"+
+								  "技能1:"+data.skill1.Name+"\n"+
+								  "--效果:"+data.skill1.Introduction+"\n"+
+								  "--MP消耗/AP消耗/範圍:"+data.skill1.MPcost+"/"+data.skill1.APcost+"/"+data.skill1.range+"\n"+
+								  "技能2:"+data.skill2.Name+"\n"+
+								  "--效果:"+data.skill2.Introduction+"\n"+
+								  "--MP消耗/AP消耗/範圍:"+data.skill2.MPcost+"/"+data.skill2.APcost+"/"+data.skill2.range+"\n"+
+								  "技能3:"+data.skill3.Name+"\n"+
+								  "--效果:"+data.skill3.Introduction+"\n"+
+								  "--MP消耗/AP消耗/範圍:"+data.skill3.MPcost+"/"+data.skill3.APcost+"/"+data.skill3.range+"\n"+
+								  "被動技能1:"+data.Passive_skill1+"\n"+
+								  "被動技能2:"+data.Passive_skill2+"\n"+
+								  "被動技能3:"+data.Passive_skill3+"\n";
+					re_message.Line_reply(replyToken, rply);
 				}
-				else{
-					for(let card of data){
-						if(card.ID==mainMsg[1]){
-							rply.text="["+card.ID+"]"+card.Name+"\n"+
-									  "簡介:"+card.Introduction+"\n"+
-									  "HP/MP/AP/ATK:"+card.HP+"/"+card.MP+"/"+card.AP+"/"+card.ATK+"\n"+
-									  "移動消耗的AP/攻擊消耗的AP:"+card.MoveAP+"/"+card.AttackAP+"\n"+
-									  "攻擊距離:"+card.AttackRange+"\n"+
-									  "技能1:"+card.skill1.Name+"\n"+
-									  "--效果:"+card.skill1.Introduction+"\n"+
-									  "--MP消耗/AP消耗/範圍:"+card.skill1.MPcost+"/"+card.skill1.APcost+"/"+card.skill1.range+"\n"+
-									  "技能2:"+card.skill2.Name+"\n"+
-									  "--效果:"+card.skill2.Introduction+"\n"+
-									  "--MP消耗/AP消耗/範圍:"+card.skill2.MPcost+"/"+card.skill2.APcost+"/"+card.skill2.range+"\n"+
-									  "技能3:"+card.skill3.Name+"\n"+
-									  "--效果:"+card.skill3.Introduction+"\n"+
-									  "--MP消耗/AP消耗/範圍:"+card.skill3.MPcost+"/"+card.skill3.APcost+"/"+card.skill3.range+"\n"+
-									  "被動技能1:"+card.Passive_skill1+"\n"+
-									  "被動技能2:"+card.Passive_skill2+"\n"+
-									  "被動技能3:"+card.Passive_skill3+"\n";
-						}
-					}
-				}
-			re_message.Line_reply(replyToken, rply);
-		});
+			});
+			
+		}
 	});
 }
 
