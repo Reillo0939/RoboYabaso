349c9ae52d782f3585cdbe4c9c60c27462462f3b
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2019-11-23 00:32:08 +0800

    Add files via upload

---
+++ system/Card/test.js
@@ -0,0 +1,35 @@
+var re_message = require('../message.js');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+// Connection URL
+const url = process.env.DB_URL;
+// Database Name
+const dbName = 'dream-realm-v2';
+// Create a new MongoClient
+const Mongoclient = new MongoClient(url);
+
+var rply ={type : 'text'};
+let msgSplitor = (/\S+/ig);	
+
+function test(UserId,UserName,Message,replyToken){
+	let mainMsg = Message.match(msgSplitor);
+	Mongoclient.connect(function(err) {
+		assert.equal(null, err);
+		//console.log("Connected successfully to server");
+		Mongoclient.db(dbName).collection('system').findOne({name:"test"}).then((data)=> {
+			var all=data.N+data.R+data.SR+data.SSR+data.UR;
+			data.UR++;
+			Mongoclient.db(dbName).collection('system').update({name:"test"},{"$set":data}, function(err, r) {
+				assert.equal(null, err);
+			});
+			//rply.text="["+data.ID+"]"+data.Race+"-"+data.Name+"\n";
+			//re_message.Line_reply(replyToken, rply);
+		});
+		
+	});
+}
+
+
+module.exports = {
+	test:test
+};
