e05cabe537c0fc89fc9eae120193280850ce74b9
Reillo0939 <37070660+Reillo0939@users.noreply.github.com>
2019-12-02 15:36:57 +0800

    Update nAnB.js

--- system/LittleGame/nAnB.js
+++ system/LittleGame/nAnB.js
@@ -120,17 +120,17 @@ function Game(UserId,UserName,Message,replyToken){
 							delete_play(player.UserId);
 							re_message.Line_reply(replyToken, rply);
 						}
-						else if(player.money_in>5 && player.count==8){
+						else if(player.money_in>5 && player.money_in<=10 && player.count==8){
 							rply.text=player.NickName+" 遊戲失敗\n答案為"+player.answer[0]+player.answer[1]+player.answer[2]+player.answer[3]+"\n繼續努力吧";
 							delete_play(player.UserId);
 							re_message.Line_reply(replyToken, rply);
 						}
-						else if(player.money_in>10 && player.count==6){
+						else if(player.money_in>10 && player.money_in<=50&& player.count==6){
 							rply.text=player.NickName+" 遊戲失敗\n答案為"+player.answer[0]+player.answer[1]+player.answer[2]+player.answer[3]+"\n繼續努力吧";
 							delete_play(player.UserId);
 							re_message.Line_reply(replyToken, rply);
 						}
-						else if(player.money_in>50 && player.count==4){
+						else if(player.money_in>50 && player.money_in<=250 && player.count==4){
 							rply.text=player.NickName+" 遊戲失敗\n答案為"+player.answer[0]+player.answer[1]+player.answer[2]+player.answer[3]+"\n繼續努力吧";
 							delete_play(player.UserId);
 							re_message.Line_reply(replyToken, rply);
